8f1d5e692d0d1596f60e71d6e95b38ec4a3e2e41
Alejandro Rivera <alejandro.rivera0309@gmail.com>
2018-09-11 19:09:20 -0500

    Disenando el view de la galeria principal

---
+++ views/index.view.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<title>Galeria</title>
+</head>
+<body>
+	<header>
+		<div class="contenedor">
+			<h1 class="titulo">Mi Increible Galeria en PHP y MySQL</h1>
+		</div>
+	</header>
+	<section class="fotos">
+		<div class="contenedor">
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/1.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/2.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/3.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/4.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/5.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/6.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/7.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="imagenes/8.jpg" alt="">
+				</a>
+			</div>
+			<div class="paginacion">
+				<a href="#" class="izquierda"><i class="fa fa-long-arrow-left"> Pagina Anterior</i></a>
+				<a href="" class="derecha">Pagina Siguiente <i class="fa fa-long-arrow-right"></i></a>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		<p class="copyright">Galeria Creada Por Eduardo Alejandro Rivera</p>
+	</footer>
+</body>
+</html>

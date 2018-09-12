7a9960b4f5d07288ce3db463713c130883a3f2e5
Alejandro Rivera <alejandro.rivera0309@gmail.com>
2018-09-11 19:24:29 -0500

    Disenando el view general para cada foto

---
+++ views/foto.view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<title>Galeria</title>
+</head>
+<body>
+	<header>
+		<div class="contenedor">
+			<h1 class="titulo">Foto: 1.jpg</h1>
+		</div>
+	</header>
+
+	<div class="contenedor">
+		<div class="foto">
+			<img src="imagenes/1.jpg" alt="">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, corrupti.</p>
+			<a href="index.php" class="regresar"><i class="fa fa-long-arrow-left"> Regresar</i></a>
+		</div>
+	</div>
+
+	<footer>
+		<p class="copyright">Galeria Creada Por Eduardo Alejandro Rivera</p>
+	</footer>
+</body>
+</html>

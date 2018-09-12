d3712692abcb3c2975c111c5902b2c1fb84fc355
Alejandro Rivera <alejandro.rivera0309@gmail.com>
2018-09-12 13:43:01 -0500

    Agregando el codigo para tomar la informacion de las imagenes desde la base de datos

--- subir.php
+++ subir.php
@@ -10,7 +10,8 @@
 		$check = @getimagesize($_FILES['foto']['tmp_name']);
 		if ($check !== false) {
 			$carpeta_destino = 'fotos/';
-			$archivo_subido = $carpeta_destino . $FILES['foto']['name'];
+			$archivo_subido = $carpeta_destino . $_FILES['foto']['name'];
+			echo $archivo_subido;
 			move_uploaded_file($_FILES['foto']['tmp_name'], $archivo_subido);
 
 			$statement = $conexion -> prepare('INSERT INTO fotos (titulo, imagen, texto) VALUES (:titulo, :imagen, :texto)');
